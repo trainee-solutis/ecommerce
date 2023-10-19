f1b1cf1e2555459be707864d0749d2c678043584
Matheus Araujo <matheus10_araujo@hotmail.com>
2023-10-19 10:46:39 -0300

    add product-slider-card structure

---
+++ src/app/components/product-slider-card/product-slider-card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-slider-card',
+  templateUrl: './product-slider-card.component.html',
+  styleUrls: ['./product-slider-card.component.css']
+})
+export class ProductSliderCardComponent {
+
+}
