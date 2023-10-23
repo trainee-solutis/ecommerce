c1aa5dfc16a3fe0dd5ac08922c14ea72be9a584a
MaateusMDS <maateusmania@gmail.com>
2023-10-23 16:00:18 -0300

    feat: loaded product with rout param

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
+
 import { HomeComponent } from "./pages/home/home.component";
+import { ProductComponent } from "./pages/product/product.component";
 
 const routes: Routes = [
   {
-    path: '',
+    path: "",
     component: HomeComponent
+  },
+  {
+    path: "product/:id",
+    component: ProductComponent
   }
 ];
 
--- src/app/pages/product/product.component.ts
+++ src/app/pages/product/product.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { Product } from "app/models/product";
+import { ProductsService } from "app/services/products.service";
+// import { Observable } from "rxjs";
 
 @Component({
-  selector: 'app-product',
-  templateUrl: './product.component.html',
-  styleUrls: ['./product.component.css']
+  selector: "app-product",
+  templateUrl: "./product.component.html",
+  styleUrls: ["./product.component.css"]
 })
-export class ProductComponent {
+export class ProductComponent implements OnInit {
+
+  private id: number;
+  product!: Product;
+
+  constructor(private route: ActivatedRoute, private productService: ProductsService) {
+    this.id = this.route.snapshot.params["id"];
+  }
+
+  ngOnInit() {
+    this.productService.getProduct(this.id).subscribe((product) => {
+      this.product = product;
+    });
+  }
+
+
 
 }
