0f515b56b04ec64f4accd6469fa0e75b813efe61
silvaalejesus <silvaalejesus@gmail.com>
2023-10-24 15:00:46 -0300

    add busca de produtos na home

--- src/app/components/searchBar/search-bar.component.ts
+++ src/app/components/searchBar/search-bar.component.ts
@@ -1,5 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, OperatorFunction, distinctUntilChanged, filter, map } from 'rxjs';
 
+type State = { id: number; name: string };
+
+// substituir esse array pelos dados do back-end
+const states: State[] = [
+  { id: 0, name: 'Alabama' },
+  { id: 1, name: 'Alaska' },
+  { id: 2, name: 'American Samoa' },
+  { id: 3, name: 'Arizona' },
+  { id: 4, name: 'Arkansas' },
+  { id: 5, name: 'California' },
+  { id: 6, name: 'Colorado' },
+  { id: 7, name: 'Connecticut' },
+  { id: 8, name: 'Delaware' },
+  { id: 9, name: 'District Of Columbia' },
+  { id: 10, name: 'Federated States Of Micronesia' }
+];
 @Component({
   selector: 'app-search-bar',
   templateUrl: './search-bar.component.html',
@@ -18,4 +35,21 @@ export class SearchBarComponent implements OnInit {
         this.texto = 'Caro cliente, o que procura hoje?';
       }
     }
+
+    public model!: State;
+
+  formatter = (state: State) => state.name;
+
+  search: OperatorFunction<string, readonly { id:number; name: string }[]> = (
+    text$: Observable<string>
+  ) =>
+    text$.pipe(
+      distinctUntilChanged(),
+      filter((term) => term.length >= 2),
+      map((term) =>
+        states
+          .filter((state) => new RegExp(term, 'mi').test(state.name))
+          .slice(0, 10)
+      )
+    );
 }
