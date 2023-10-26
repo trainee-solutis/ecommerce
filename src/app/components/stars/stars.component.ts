5c1fe538ce6c6c61c6ba2201b68a1540ac6171db
4ntonio19 <vgoncalvescarvalho@outlook.com>
2023-10-26 20:58:49 -0300

    feat/rating done

--- src/app/components/stars/stars.component.ts
+++ src/app/components/stars/stars.component.ts
@@ -1,6 +1,7 @@
 import { NgIf } from "@angular/common";
-import { Component, OnInit } from "@angular/core";
+import { Component, Input, OnInit } from "@angular/core";
 import { NgbRatingModule } from "@ng-bootstrap/ng-bootstrap";
+import { Product, Rating } from "app/models/product";
 
 @Component({
   selector: "app-stars",
@@ -12,19 +13,25 @@ import { NgbRatingModule } from "@ng-bootstrap/ng-bootstrap";
 export class StarsComponent implements OnInit {
 
   currentRate = 0;
-  ratings: number[] = [
-    4.2, 3.7, 2.5, 1.8, 4.0, 3.3, 1.9,
-    3.6, 2.7, 4.5, 2.2, 3.1, 4.8, 1.4, 5.0, 2.8, 3.9, 4.3,
-    1.7, 3.4, 4.1, 2.3, 4.7, 3.8, 2.9, 3.2, 4.6, 1.5, 2.0, 3.0,
-    1.6, 4.4, 2.4, 1.3, 4.9, 3.5, 2.1, 1.2,
-    4.0, 3.0, 1.0, 2.0, 5.0, 4.5, 3.5, 2.5, 1.5, 3.5, 4.5, 5];
-  totalRating = this.ratings.length;
-  calcularRating(): number {
-    const totalRating = this.ratings.reduce((acc, curr) => acc + curr, 0);
-    const averageRating = totalRating / this.ratings.length;
-    return Number(averageRating.toFixed(2));
+  totalReviews?: number;
+  @Input() product?: Product;
+  ratings?: Rating[];
+
+  constructor() {
   }
+
   ngOnInit(): void {
+    this.ratings = this.product?.rating;
     this.currentRate = this.calcularRating();
+    this.totalReviews = this.ratings?.length ?? 0;
+  }
+
+  calcularRating(): number {
+    if (!this.ratings || this.ratings.length === 0) {
+      return 0;
+    }
+    const sum = this.ratings.reduce((previous, current) => previous + current.stars, 0);
+    const avarage = sum/this.ratings.length;
+    return Number(avarage.toFixed(2));
   }
 }
