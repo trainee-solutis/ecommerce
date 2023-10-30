2792d526fefef09d30de9bc3d6d19c933e2e477d
MaateusMDS <maateusmania@gmail.com>
2023-10-30 16:29:10 -0300

    perf: search bar style & performance

--- src/app/components/searchBar/search-bar.component.ts
+++ src/app/components/searchBar/search-bar.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Product } from 'app/models/product';
 import { ProductsService } from 'app/services/products.service';
 import { Observable } from 'rxjs';
-import { distinctUntilChanged, filter, switchMap } from 'rxjs/operators';
+import { distinctUntilChanged, filter, map, switchMap } from 'rxjs/operators';
 
 @Component({
   selector: "app-search-bar",
@@ -11,10 +11,13 @@ import { distinctUntilChanged, filter, switchMap } from 'rxjs/operators';
   styleUrls: ["./search-bar.component.css"]
 })
 export class SearchBarComponent implements OnInit {
-  texto: string = '';
-  searchTerm: string = '';
 
-  constructor(private productsService: ProductsService, private router: Router) {}
+  texto: string = '';
+  searchTerm!: string | Product;
+
+  constructor(private productsService: ProductsService, private router: Router, private route: ActivatedRoute) {
+    this.searchTerm = '';
+  }
 
   ngOnInit(): void {
     const token = localStorage.getItem('token');
@@ -25,22 +28,30 @@ export class SearchBarComponent implements OnInit {
     } else {
       this.texto = 'Caro cliente, o que procura hoje?';
     }
-
-    this.searchTerm = '';
   }
 
-  formatter = (produto?: Product) => produto?.name || '';
+  formatter = (product: Product) => product.name;
 
   search = (text$: Observable<string>): Observable<readonly Product[]> =>
-    text$.pipe(
-      distinctUntilChanged(),
-      filter((term: string) => term.length >= 3),
-      switchMap((term: string) => this.productsService.getProductByName(term))
-    );
+  text$.pipe(
+    distinctUntilChanged(),
+    filter((term: string) => term.length === 3),
+    switchMap((term: string) => {
+      return this.productsService.getProductByName(term).pipe(
+        map((products: Product[]) => products.slice(0, 10))
+      );
+    })
+  );
 
-  goToProductPage(product: any): void {
-    try {
-      this.router.navigate([`/product/${product.id}`]);
-    } catch (error) {}
+  goToProductPage() {
+    if (typeof this.searchTerm !== 'string' && this.searchTerm.id) {
+      this.router.navigate([`/product/${this.searchTerm.id}`]);
+    } else if (typeof this.searchTerm === 'string' && this.searchTerm.length >= 3) {
+      this.productsService.getProductByName(this.searchTerm).subscribe((products) => {
+        if (products.length > 0) {
+          this.router.navigate([`/product/${products[0].id}`]);
+        }
+      });
+    }
   }
 }
