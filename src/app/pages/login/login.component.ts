a66f651bd1e4a2b6da190198482044c1161b8ca7
4ntonio19 <vgoncalvescarvalho@outlook.com>
2023-11-14 16:30:48 -0300

    auth service and login logout done

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthenticatorService } from 'app/services/authenticator.service';
 
 @Component({
   selector: 'app-login',
@@ -7,20 +9,33 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
-  constructor(private formBuilder: FormBuilder) { }
-  formulario!: FormGroup
-
+  constructor(private formBuilder: FormBuilder, private service: AuthenticatorService, private router: Router) { }
+  formulario!: FormGroup;
   hide = true;
   FormBuilder: any;
 
   ngOnInit(): void {
     this.formulario = this.formBuilder.group({
       email: [null, [Validators.required, Validators.email]],
-      senha: [null, [Validators.required, Validators.minLength(6)]]
+      senha: [null, [Validators.required, Validators.minLength(4)]]
     })
   }
 
   onSubmit() {
     console.log(this.formulario)
+  }
+
+  authenticator(){
+    if(this.formulario.valid){
+      const email = this.formulario.value.email;
+      const password = this.formulario.value.senha;
+
+      this.service.login(email,password).then(() => {
+        alert("Login feito com sucesso");
+        this.router.navigate(['/home'])
+      }).catch((error) => {
+        alert("Erro ao fazer login");
+      });
+    }
   }
 }
