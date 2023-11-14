47d0aeefb7daca3e1147b7a0df5626b486630046
victor <victordosanjos19@gmail.com>
2023-11-13 21:20:03 -0300

    Refactor password change form to use form validation and add match password validator

--- src/app/components/access-data-page/access-data-page.component.ts
+++ src/app/components/access-data-page/access-data-page.component.ts
@@ -1,17 +1,41 @@
+import { OnInit } from '@angular/core';
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormGroup, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
+import { matchPassword } from './matchpassword.validator';
 
 @Component({
   selector: 'app-access-data-page',
   templateUrl: './access-data-page.component.html',
   styleUrls: ['./access-data-page.component.css']
 })
-export class AccessDataPageComponent {
+export class AccessDataPageComponent implements OnInit {
 
-  hide = true;
+  passwordForm!: FormGroup;
+  submitted = false;
+
   hideOldPassword = true;
   hideNewPassword = true;
   hideConfirmPassword = true;
 
+  constructor(private formBuilder: FormBuilder){
+  }
+
+  ngOnInit(): void {
+
+    this.passwordForm = this.formBuilder.group({
+      oldPassword: ['', Validators.required],
+      newPassword: ['', [Validators.required, Validators.minLength(8)]],
+      confirmPassword: ['', [Validators.required, Validators.minLength(8)]],
+    });
+
+    this.passwordForm.setValidators(matchPassword);
+  }
+
+
+  changePassword(){
+    this.submitted = true;
+    console.log('change password', this.passwordForm.value)
+  };
 
 }
+
