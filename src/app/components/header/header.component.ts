d88d6c74aef5df9943cc31f911eb6fa4ae36276f
4ntonio19 <vgoncalvescarvalho@outlook.com>
2023-11-14 16:30:48 -0300

    auth service and login logout done

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,20 +1,29 @@
-import { Component, EventEmitter, Input, OnInit, Output } from "@angular/core";
+import { AfterViewInit, Component, EventEmitter, Input, OnInit, Output } from "@angular/core";
 import { MatDialog } from "@angular/material/dialog";
 import { ModalComponent } from "@components/modal/modal.component";
 import { BasketService } from "app/services/basket/basket.service";
 import { MatMenu } from "@angular/material/menu";
+import { AuthenticatorService } from "app/services/authenticator.service";
 
 @Component({
   selector: "app-header",
   templateUrl: "./header.component.html",
   styleUrls: ["./header.component.css"],
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, AfterViewInit {
   quantity: number;
   isLogged: boolean = false;
 
-  constructor(public dialog: MatDialog, public basketService: BasketService) {
+  constructor(public dialog: MatDialog, public basketService: BasketService, public authService: AuthenticatorService) {
     this.quantity = 0;
+  }
+  ngAfterViewInit(): void {
+    this.authService.isLogged().then((data) => {
+      if(data){
+        this.isLogged = true
+      }
+      console.log(data)}
+    );
   }
 
   openDialog(valorRecebido: number) {
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,6 @@ export const environment = {
   api_url: "http://localhost:3000",
   api_email: "http://localhost:3001/send-email",
   key_CEP: "5d19ccda5002fee70f27aaad232af7651107e098",
-  jwt_secret: "cc7e0d44fd473002f1c42167459001140ec6389b7353f8088f4d9a95f2f596f3",
+  jwt_secret: "YW5ndWxhcg==",
   key_stripe: "pk_test_51K2MeqIuHlDXGl6Easdi54PuYFaBRELQE3j6HefWHGHgj7KiJqtdPgqWSdN0pxeEAAx5JWjAujAjmfn1McleYxRM005HdUHj8q",
 };
