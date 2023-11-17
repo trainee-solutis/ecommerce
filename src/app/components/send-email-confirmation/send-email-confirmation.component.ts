ec315fd159323cc52dbb1ee93a070d6ec0407e69
Guilherme <guilhermevaccaro6@gmail.com>
2023-11-17 16:49:44 -0300

    feat/modal-send-email-confirmation

---
+++ src/app/components/send-email-confirmation/send-email-confirmation.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-send-email-confirmation',
+  templateUrl: './send-email-confirmation.component.html',
+  styleUrls: ['./send-email-confirmation.component.css']
+})
+export class SendEmailConfirmationComponent {
+
+}
